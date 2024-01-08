b6c05da68f0f8133323c5e927beaab18a2a50102
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2024-01-08 19:01:51 +0200

    Implement environment and char art

--- Player/player.gd
+++ Player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal block_destroyed()
 
 var SPEED = 100
 var direction = Vector2.ZERO
@@ -11,13 +12,22 @@ func _physics_process(delta):
 	velocity = direction * SPEED
 	var collision = move_and_collide(velocity * delta)
 	if collision and collision.get_collider() is TileMap:
-		var tilemap = collision.get_collider()
-		var pos = tilemap.local_to_map(collision.get_position())
+		var tilemap := collision.get_collider() as TileMap
+		var pos = tilemap.local_to_map(collision.get_position() - collision.get_normal() * 1.0)
 		#coordinates for ores
 		var coords = tilemap.get_cell_atlas_coords(0,pos)
-		if coords != Vector2i(0,0):
-			#Mining Ores does what (Coords for Ores on Atlas TileMap)
-			print(coords)
-		tilemap.erase_cell(0,pos)
+		tilemap.erase_cell(0, pos)
+		if tilemap.name != "Dirt": return
+		tilemap.erase_cell(1, pos)
+		for pos_offset in [
+			pos + Vector2i.UP,
+			pos + Vector2i.DOWN,
+			pos + Vector2i.LEFT,
+			pos + Vector2i.RIGHT,
+		]:
+			if tilemap.get_cell_atlas_coords(0, pos_offset) != Vector2i(-1, -1):
+				tilemap.set_cell(1, pos_offset, 0, Vector2i(randi() % 3, 0))
+
+		block_destroyed.emit()
 		
 		
---
+++ Player/player_animation_manager.gd
@@ -0,0 +1,49 @@
+extends Node
+
+@export var controller : CharacterBody2D
+@export var eye_l : Node2D
+@export var eye_r : Node2D
+@export var hands_item : Node2D
+@export var backpack : Node2D
+@export var hscale : Node2D
+@export var eye_radius := 16.0
+@export var drill_shake := 12.0
+@export var drill_shake_time := 0.2
+@export var backpack_shake := 12.0
+@export var anim : AnimationTree
+
+var look_vector := Vector2.ZERO
+var block_destroyed_timer : Timer
+
+
+func _ready():
+	anim.active = true
+	block_destroyed_timer = Timer.new()
+	block_destroyed_timer.one_shot = true
+	block_destroyed_timer.timeout.connect(func():
+		anim["parameters/claw/playback"].travel(&"idle")
+	)
+	add_child(block_destroyed_timer)
+
+
+func _process(delta : float):
+	var rel_velocity = controller.velocity / controller.SPEED
+
+	if rel_velocity.x > 0.0: hscale.scale.x = +1.0
+	if rel_velocity.x < 0.0: hscale.scale.x = -1.0
+
+	rel_velocity.x *= hscale.scale.x
+	look_vector = look_vector.lerp(rel_velocity, 0.1)
+
+	eye_l.position = look_vector * eye_radius
+	eye_r.position = look_vector * eye_radius
+	hands_item.rotation = lerp_angle(hands_item.rotation, (look_vector * Vector2(1.0, 0.5)).angle(), 0.2)
+	if block_destroyed_timer.time_left > 0.0:
+		hands_item.position = Vector2(randf() - 0.5, randf() - 0.5) * drill_shake
+
+	backpack.position = Vector2(randf() - 0.5, randf() - 0.5) * backpack_shake
+
+
+func _on_player_block_destroyed():
+	block_destroyed_timer.start(drill_shake_time)
+	anim["parameters/claw/playback"].travel(&"dig")
