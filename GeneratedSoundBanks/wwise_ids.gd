79639adbf793ad6df0b47d488e99b62d4979d5d8
Purisen <gmarcelsmith@gmail.com>
2024-01-09 16:03:44 -0800

    WWise integration and Digging SFX

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,64 @@
+class_name AK
+
+class EVENTS:
+
+	const DRILL_MINING = 737866587
+	const STOP_DRILL_MINING = 2118051910
+
+	const _dict = {
+		"Drill_Mining": DRILL_MINING,
+		"Stop_Drill_Mining": STOP_DRILL_MINING
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const MAIN = 3161908922
+
+	const _dict = {
+		"Init": INIT,
+		"Main": MAIN
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
