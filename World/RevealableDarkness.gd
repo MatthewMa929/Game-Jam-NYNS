120d6eb5db7b629cc5019e56f40fb95a18da8e76
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2024-01-13 18:49:25 +0200

    Implement revealable darkness

--- World/RevealableDarkness.gd
+++ World/RevealableDarkness.gd
@@ -4,8 +4,7 @@ extends TileMap
 
 var reveal_radius = 256.0
 
-var pregenerate_depth = 256.0
-var pregenerate_width = 1024.0
+var pregenerate_depth = 2048.0
 
 var revealer_last_pos = Vector2()
 var revealer_nex_gen_threshold = 0.0
@@ -15,9 +14,19 @@ func _process(delta):
 	if revealer_last_pos == revealer.position: return
 	revealer_last_pos = revealer.position
 
-	# if revealer_last_pos.y > revealer_nex_gen_threshold:
-	# 	for x in $"../Ores".WIDTH * $"../Ores".tile_set.tile_size.x / tile_set.tile_size.x:
+	if revealer_last_pos.y > revealer_nex_gen_threshold - pregenerate_depth * 0.5:
+		for x in $"../Ores".WIDTH * $"../Ores".tile_set.tile_size.x / tile_set.tile_size.x:
+			var tile_start = revealer_nex_gen_threshold / tile_set.tile_size.y
+			for y in pregenerate_depth / tile_set.tile_size.y:
+				set_cell(0, Vector2i(x, tile_start + y), 0, Vector2i.ZERO)
 
-	# 	revealer_nex_gen_threshold += pregenerate_depth
+		revealer_nex_gen_threshold += pregenerate_depth
 
-	# pass
+	var erase_center = revealer_last_pos / Vector2(tile_set.tile_size)
+	var reveal_radius_tiles = Vector2(reveal_radius, reveal_radius) / Vector2(tile_set.tile_size)
+	for x in reveal_radius_tiles.x + reveal_radius_tiles.x:
+		for y in reveal_radius_tiles.y + reveal_radius_tiles.y:
+			if Vector2(x, y).distance_squared_to(reveal_radius_tiles) <= reveal_radius_tiles.x * reveal_radius_tiles.x:
+				erase_cell(0, erase_center - reveal_radius_tiles + Vector2(x, y))
+
+	erase_cell(0, erase_center)
