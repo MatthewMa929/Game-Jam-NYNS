5fca4a81781da2e36e61cf01d5a729fb0622c474
mm11762 <MatthewMa929@Gmail.com>
2024-01-14 06:50:36 -0500

    wwise fixing

---
+++ wwise_fixer.gd
@@ -0,0 +1,23 @@
+extends Node
+
+class_name Wwise
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+static func post_event(p, e):
+	pass
+
+static func register_game_obj(p, e):
+	pass
+
+static func set_rtpc_value(p, e, c):
+	pass
+
+static func set_state(c, a):
+	pass
