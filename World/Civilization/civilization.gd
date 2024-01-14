b5c9661b30ede5ded908f95d6bb59af97e72d0f4
Goosy-Goose <selina.gong0406@gmail.com>
2024-01-13 17:29:29 -0800

    Update civilization.gd

--- World/Civilization/civilization.gd
+++ World/Civilization/civilization.gd
@@ -4,8 +4,22 @@ const CIV_WIDTH = 1828
 const CIV_HEIGHT = 1235
 
 @onready var rng = RandomNumberGenerator.new()
+@onready var bg = $Background
 @onready var workshop = $Workshop
 @onready var qtmole = $SurpriseMole
+
+var civ_textures := [
+	preload("res://Sprites/Civilization/civ1.png"),
+	preload("res://Sprites/Civilization/civ3.png"),
+	preload("res://Sprites/Civilization/civ7.png"),
+	preload("res://Sprites/Civilization/civ8.png"),
+	
+	preload("res://Sprites/Civilization/civ2.png"),
+	preload("res://Sprites/Civilization/civ4.png"),
+	preload("res://Sprites/Civilization/civ5.png"),
+	preload("res://Sprites/Civilization/civ6.png"),
+	preload("res://Sprites/Civilization/civ9.png")
+] as Array[Texture]
 
 
 # 450px tall, 750px wide
@@ -15,7 +29,6 @@ func _ready():
 	# set the x and y to a random val   // x clamped (600, 2200), y every 3500
 	
 	var civ_position = Vector2i(0,0)
-	
 	var bg_variant = randi_range(0,8)
 	_set_civ_variant(bg_variant)
 	 
@@ -35,15 +48,17 @@ func _on_tile_delete_area_entered(area):
 
 
 func _set_civ_variant(bg_var):
+	# sets background variant
+	bg.texture = civ_textures[bg_var]
 	
-	# chooses the workshop location
+	# sets the workshop location
 	var ws_variant = randi_range(0,3)
-	if (bg_var <=2):
+	if (bg_var <=3):
 		ws_variant = randi_range(0,2)
 	workshop.region_rect = Rect2(CIV_WIDTH*ws_variant, 0, CIV_WIDTH, CIV_HEIGHT)
 	
 	# qtmole ;P <3
-	if (rng.randf() < 0.9):
+	if (rng.randf() < 0.1):
 		qtmole.visible = true
 	else:
 		qtmole.visible = false
